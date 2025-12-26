d7d59ac5576a78ab1074b72e20701da3b83556e1
LotusBoard Admin <admin@lotusboard.local>
2025-12-26 17:49:31 +0330

    Add 10 task variants for array operations
    Redesign form with task selection using RadioButtons. Implement all 10 tasks: count positive/negative, split by indices, find average of first negative and last positive, count elements before max positive, swap first and last positive elements, and tasks for 10/20 element arrays including sum calculations and index finding.

--- pm-labs/Form1.Designer.cs
+++ pm-labs/Form1.Designer.cs
@@ -40,6 +40,18 @@
             btnClear = new Button();
             saveFileDialog1 = new SaveFileDialog();
             openFileDialog1 = new OpenFileDialog();
+            grpTask = new GroupBox();
+            rbTask1 = new RadioButton();
+            rbTask2 = new RadioButton();
+            rbTask3 = new RadioButton();
+            rbTask4 = new RadioButton();
+            rbTask5 = new RadioButton();
+            rbTask6 = new RadioButton();
+            rbTask7 = new RadioButton();
+            rbTask8 = new RadioButton();
+            rbTask9 = new RadioButton();
+            rbTask10 = new RadioButton();
+            grpTask.SuspendLayout();
             SuspendLayout();
 
             lblN.AutoSize = true;
@@ -62,56 +74,145 @@
             btnGenerate.UseVisualStyleBackColor = true;
             btnGenerate.Click += btnGenerate_Click;
 
+            grpTask.Controls.Add(rbTask10);
+            grpTask.Controls.Add(rbTask9);
+            grpTask.Controls.Add(rbTask8);
+            grpTask.Controls.Add(rbTask7);
+            grpTask.Controls.Add(rbTask6);
+            grpTask.Controls.Add(rbTask5);
+            grpTask.Controls.Add(rbTask4);
+            grpTask.Controls.Add(rbTask3);
+            grpTask.Controls.Add(rbTask2);
+            grpTask.Controls.Add(rbTask1);
+            grpTask.Location = new Point(20, 60);
+            grpTask.Name = "grpTask";
+            grpTask.Size = new Size(560, 180);
+            grpTask.TabIndex = 3;
+            grpTask.TabStop = false;
+            grpTask.Text = "Выберите задание";
+
+            rbTask1.AutoSize = true;
+            rbTask1.Checked = true;
+            rbTask1.Location = new Point(15, 25);
+            rbTask1.Name = "rbTask1";
+            rbTask1.Size = new Size(480, 19);
+            rbTask1.TabIndex = 0;
+            rbTask1.TabStop = true;
+            rbTask1.Text = "1. Количество отрицательных, положительных и среднее арифметическое";
+
+            rbTask2.AutoSize = true;
+            rbTask2.Location = new Point(15, 45);
+            rbTask2.Name = "rbTask2";
+            rbTask2.Size = new Size(470, 19);
+            rbTask2.TabIndex = 1;
+            rbTask2.Text = "2. Разделить на два массива (четные/нечетные индексы) и найти суммы";
+
+            rbTask3.AutoSize = true;
+            rbTask3.Location = new Point(15, 65);
+            rbTask3.Name = "rbTask3";
+            rbTask3.Size = new Size(480, 19);
+            rbTask3.TabIndex = 2;
+            rbTask3.Text = "3. Среднее арифметическое первого отрицательного и последнего положительного";
+
+            rbTask4.AutoSize = true;
+            rbTask4.Location = new Point(15, 85);
+            rbTask4.Name = "rbTask4";
+            rbTask4.Size = new Size(500, 19);
+            rbTask4.TabIndex = 3;
+            rbTask4.Text = "4. Количество отрицательных перед наибольшим положительным";
+
+            rbTask5.AutoSize = true;
+            rbTask5.Location = new Point(15, 105);
+            rbTask5.Name = "rbTask5";
+            rbTask5.Size = new Size(410, 19);
+            rbTask5.TabIndex = 4;
+            rbTask5.Text = "5. Поменять местами первый и последний положительные элементы";
+
+            rbTask6.AutoSize = true;
+            rbTask6.Location = new Point(15, 125);
+            rbTask6.Name = "rbTask6";
+            rbTask6.Size = new Size(340, 19);
+            rbTask6.TabIndex = 5;
+            rbTask6.Text = "6. Сумма и среднее арифметическое (10 элементов)";
+
+            rbTask7.AutoSize = true;
+            rbTask7.Location = new Point(15, 145);
+            rbTask7.Name = "rbTask7";
+            rbTask7.Size = new Size(320, 19);
+            rbTask7.TabIndex = 6;
+            rbTask7.Text = "7. Сумма положительных элементов (20 элементов)";
+
+            rbTask8.AutoSize = true;
+            rbTask8.Location = new Point(290, 25);
+            rbTask8.Name = "rbTask8";
+            rbTask8.Size = new Size(260, 19);
+            rbTask8.TabIndex = 7;
+            rbTask8.Text = "8. Сумма четных элементов (20 элементов)";
+
+            rbTask9.AutoSize = true;
+            rbTask9.Location = new Point(290, 45);
+            rbTask9.Name = "rbTask9";
+            rbTask9.Size = new Size(270, 19);
+            rbTask9.TabIndex = 8;
+            rbTask9.Text = "9. Количество положительных (20 элементов)";
+
+            rbTask10.AutoSize = true;
+            rbTask10.Location = new Point(290, 65);
+            rbTask10.Name = "rbTask10";
+            rbTask10.Size = new Size(260, 19);
+            rbTask10.TabIndex = 9;
+            rbTask10.Text = "10. Индексы положительных (20 элементов)";
+
             lstArray.FormattingEnabled = true;
             lstArray.ItemHeight = 15;
-            lstArray.Location = new Point(20, 60);
+            lstArray.Location = new Point(20, 250);
             lstArray.Name = "lstArray";
-            lstArray.Size = new Size(200, 289);
-            lstArray.TabIndex = 3;
+            lstArray.Size = new Size(200, 139);
+            lstArray.TabIndex = 4;
 
-            btnAnalyze.Location = new Point(20, 365);
+            btnAnalyze.Location = new Point(20, 400);
             btnAnalyze.Name = "btnAnalyze";
             btnAnalyze.Size = new Size(200, 30);
-            btnAnalyze.TabIndex = 4;
-            btnAnalyze.Text = "Анализ массива";
+            btnAnalyze.TabIndex = 5;
+            btnAnalyze.Text = "Выполнить задание";
             btnAnalyze.UseVisualStyleBackColor = true;
             btnAnalyze.Click += btnAnalyze_Click;
 
             lblResults.AutoSize = true;
-            lblResults.Location = new Point(240, 60);
+            lblResults.Location = new Point(240, 250);
             lblResults.Name = "lblResults";
             lblResults.Size = new Size(70, 15);
-            lblResults.TabIndex = 5;
+            lblResults.TabIndex = 6;
             lblResults.Text = "Результаты:";
 
-            txtResults.Location = new Point(240, 85);
+            txtResults.Location = new Point(240, 270);
             txtResults.Multiline = true;
             txtResults.Name = "txtResults";
             txtResults.ReadOnly = true;
             txtResults.ScrollBars = ScrollBars.Vertical;
-            txtResults.Size = new Size(340, 180);
-            txtResults.TabIndex = 6;
+            txtResults.Size = new Size(340, 119);
+            txtResults.TabIndex = 7;
 
-            btnSave.Location = new Point(240, 285);
+            btnSave.Location = new Point(240, 400);
             btnSave.Name = "btnSave";
             btnSave.Size = new Size(160, 30);
-            btnSave.TabIndex = 7;
+            btnSave.TabIndex = 8;
             btnSave.Text = "Сохранить в файл";
             btnSave.UseVisualStyleBackColor = true;
             btnSave.Click += btnSave_Click;
 
-            btnLoad.Location = new Point(420, 285);
+            btnLoad.Location = new Point(420, 400);
             btnLoad.Name = "btnLoad";
             btnLoad.Size = new Size(160, 30);
-            btnLoad.TabIndex = 8;
+            btnLoad.TabIndex = 9;
             btnLoad.Text = "Загрузить из файла";
             btnLoad.UseVisualStyleBackColor = true;
             btnLoad.Click += btnLoad_Click;
 
-            btnClear.Location = new Point(240, 330);
+            btnClear.Location = new Point(240, 440);
             btnClear.Name = "btnClear";
             btnClear.Size = new Size(340, 30);
-            btnClear.TabIndex = 9;
+            btnClear.TabIndex = 10;
             btnClear.Text = "Очистить";
             btnClear.UseVisualStyleBackColor = true;
             btnClear.Click += btnClear_Click;
@@ -126,7 +227,7 @@
 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(600, 410);
+            ClientSize = new Size(600, 485);
             Controls.Add(btnClear);
             Controls.Add(btnLoad);
             Controls.Add(btnSave);
@@ -134,6 +235,7 @@
             Controls.Add(lblResults);
             Controls.Add(btnAnalyze);
             Controls.Add(lstArray);
+            Controls.Add(grpTask);
             Controls.Add(btnGenerate);
             Controls.Add(txtN);
             Controls.Add(lblN);
@@ -142,6 +244,8 @@
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Выполнил студент группы ИСПП-242 Кузнецова В.Б.";
+            grpTask.ResumeLayout(false);
+            grpTask.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -160,5 +264,16 @@
         private Button btnClear;
         private SaveFileDialog saveFileDialog1;
         private OpenFileDialog openFileDialog1;
+        private GroupBox grpTask;
+        private RadioButton rbTask1;
+        private RadioButton rbTask2;
+        private RadioButton rbTask3;
+        private RadioButton rbTask4;
+        private RadioButton rbTask5;
+        private RadioButton rbTask6;
+        private RadioButton rbTask7;
+        private RadioButton rbTask8;
+        private RadioButton rbTask9;
+        private RadioButton rbTask10;
     }
 }
--- pm-labs/Form1.cs
+++ pm-labs/Form1.cs
@@ -44,6 +44,32 @@ namespace pm_labs
                 return;
             }
 
+            txtResults.Clear();
+
+            if (rbTask1.Checked)
+                Task1();
+            else if (rbTask2.Checked)
+                Task2();
+            else if (rbTask3.Checked)
+                Task3();
+            else if (rbTask4.Checked)
+                Task4();
+            else if (rbTask5.Checked)
+                Task5();
+            else if (rbTask6.Checked)
+                Task6();
+            else if (rbTask7.Checked)
+                Task7();
+            else if (rbTask8.Checked)
+                Task8();
+            else if (rbTask9.Checked)
+                Task9();
+            else if (rbTask10.Checked)
+                Task10();
+        }
+
+        private void Task1()
+        {
             int negativeCount = 0;
             int positiveCount = 0;
             int sum = 0;
@@ -62,6 +88,232 @@ namespace pm_labs
             txtResults.Text = $"Количество отрицательных чисел: {negativeCount}\r\n";
             txtResults.Text += $"Количество положительных чисел: {positiveCount}\r\n";
             txtResults.Text += $"Среднее арифметическое всех чисел: {average:F2}";
+        }
+
+        private void Task2()
+        {
+            int[] evenIndices = new int[array.Length / 2 + array.Length % 2];
+            int[] oddIndices = new int[array.Length / 2];
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenIndices[i / 2] = array[i];
+                    evenSum += array[i];
+                }
+                else
+                {
+                    oddIndices[i / 2] = array[i];
+                    oddSum += array[i];
+                }
+            }
+
+            txtResults.Text = "Массив с четными индексами: ";
+            txtResults.Text += string.Join(", ", evenIndices) + "\r\n";
+            txtResults.Text += $"Сумма элементов с четными индексами: {evenSum}\r\n\r\n";
+            txtResults.Text += "Массив с нечетными индексами: ";
+            txtResults.Text += string.Join(", ", oddIndices) + "\r\n";
+            txtResults.Text += $"Сумма элементов с нечетными индексами: {oddSum}";
+        }
+
+        private void Task3()
+        {
+            int firstNegative = int.MaxValue;
+            int lastPositive = int.MinValue;
+            bool foundNegative = false;
+            bool foundPositive = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0 && !foundNegative)
+                {
+                    firstNegative = array[i];
+                    foundNegative = true;
+                }
+                if (array[i] > 0)
+                {
+                    lastPositive = array[i];
+                    foundPositive = true;
+                }
+            }
+
+            if (!foundNegative || !foundPositive)
+            {
+                txtResults.Text = "В массиве нет отрицательных или положительных элементов";
+                return;
+            }
+
+            double average = (firstNegative + lastPositive) / 2.0;
+            txtResults.Text = $"Первый отрицательный элемент: {firstNegative}\r\n";
+            txtResults.Text += $"Последний положительный элемент: {lastPositive}\r\n";
+            txtResults.Text += $"Среднее арифметическое: {average:F2}";
+        }
+
+        private void Task4()
+        {
+            int maxPositive = int.MinValue;
+            int maxPositiveIndex = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0 && array[i] > maxPositive)
+                {
+                    maxPositive = array[i];
+                    maxPositiveIndex = i;
+                }
+            }
+
+            if (maxPositiveIndex == -1)
+            {
+                txtResults.Text = "В массиве нет положительных элементов";
+                return;
+            }
+
+            int negativeCount = 0;
+            for (int i = 0; i < maxPositiveIndex; i++)
+            {
+                if (array[i] < 0)
+                    negativeCount++;
+            }
+
+            txtResults.Text = $"Наибольший положительный элемент: {maxPositive}\r\n";
+            txtResults.Text += $"Его индекс: {maxPositiveIndex}\r\n";
+            txtResults.Text += $"Количество отрицательных элементов перед ним: {negativeCount}";
+        }
+
+        private void Task5()
+        {
+            int firstPositiveIndex = -1;
+            int lastPositiveIndex = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    if (firstPositiveIndex == -1)
+                        firstPositiveIndex = i;
+                    lastPositiveIndex = i;
+                }
+            }
+
+            if (firstPositiveIndex == -1 || firstPositiveIndex == lastPositiveIndex)
+            {
+                txtResults.Text = "Недостаточно положительных элементов для обмена";
+                return;
+            }
+
+            int temp = array[firstPositiveIndex];
+            array[firstPositiveIndex] = array[lastPositiveIndex];
+            array[lastPositiveIndex] = temp;
+
+            lstArray.Items.Clear();
+            foreach (int num in array)
+                lstArray.Items.Add(num);
+
+            txtResults.Text = $"Поменяны местами элементы:\r\n";
+            txtResults.Text += $"Индекс {firstPositiveIndex} (было {array[lastPositiveIndex]}, стало {array[firstPositiveIndex]})\r\n";
+            txtResults.Text += $"Индекс {lastPositiveIndex} (было {temp}, стало {array[lastPositiveIndex]})";
+        }
+
+        private void Task6()
+        {
+            if (array.Length != 10)
+            {
+                MessageBox.Show("Для этого задания массив должен содержать 10 элементов", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int sum = 0;
+            foreach (int num in array)
+                sum += num;
+
+            double average = sum / 10.0;
+
+            txtResults.Text = $"Сумма элементов: {sum}\r\n";
+            txtResults.Text += $"Среднее арифметическое: {average:F2}";
+        }
+
+        private void Task7()
+        {
+            if (array.Length != 20)
+            {
+                MessageBox.Show("Для этого задания массив должен содержать 20 элементов", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int sum = 0;
+            foreach (int num in array)
+            {
+                if (num > 0)
+                    sum += num;
+            }
+
+            txtResults.Text = $"Сумма положительных элементов: {sum}";
+        }
+
+        private void Task8()
+        {
+            if (array.Length != 20)
+            {
+                MessageBox.Show("Для этого задания массив должен содержать 20 элементов", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int sum = 0;
+            foreach (int num in array)
+            {
+                if (num % 2 == 0)
+                    sum += num;
+            }
+
+            txtResults.Text = $"Сумма четных элементов: {sum}";
+        }
+
+        private void Task9()
+        {
+            if (array.Length != 20)
+            {
+                MessageBox.Show("Для этого задания массив должен содержать 20 элементов", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int count = 0;
+            foreach (int num in array)
+            {
+                if (num > 0)
+                    count++;
+            }
+
+            txtResults.Text = $"Количество положительных элементов: {count}";
+        }
+
+        private void Task10()
+        {
+            if (array.Length != 20)
+            {
+                MessageBox.Show("Для этого задания массив должен содержать 20 элементов", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<int> indices = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                    indices.Add(i);
+            }
+
+            if (indices.Count == 0)
+            {
+                txtResults.Text = "В массиве нет положительных элементов";
+                return;
+            }
+
+            txtResults.Text = "Индексы положительных элементов:\r\n";
+            txtResults.Text += string.Join(", ", indices);
         }
 
         private void btnSave_Click(object sender, EventArgs e)
