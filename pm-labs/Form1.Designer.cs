e334ce885edec023a65027c8d99c2769c2ebac7b
LotusBoard Admin <admin@lotusboard.local>
2025-12-26 16:54:22 +0330

    Implement function calculator with extremum search
    - Add input fields for parameters a, xn, xk, xh
    - Set default values xn=3.35, xk=36.26, xh=0.2
    - Add ListBox for displaying calculation results
    - Implement three conditional functions
    - Use While loop for iterating through range
    - Calculate function values on interval [xn, xk] with step xh
    - Find minimum and maximum function values
    - Display results in table format
    - Show extremum points with coordinates
    - Add Clear and Exit buttons
    - Add documentation

--- pm-labs/Form1.Designer.cs
+++ pm-labs/Form1.Designer.cs
@@ -28,12 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblA = new Label();
+            lblXn = new Label();
+            lblXk = new Label();
+            lblXh = new Label();
+            txtA = new TextBox();
+            txtXn = new TextBox();
+            txtXk = new TextBox();
+            txtXh = new TextBox();
+            btnCalculate = new Button();
+            btnClear = new Button();
+            btnExit = new Button();
+            lstResults = new ListBox();
+            lblResults = new Label();
+            lblMin = new Label();
+            lblMax = new Label();
+            txtMin = new TextBox();
+            txtMax = new TextBox();
+            SuspendLayout();
+
+            lblA.AutoSize = true;
+            lblA.Location = new Point(20, 20);
+            lblA.Name = "lblA";
+            lblA.Size = new Size(80, 15);
+            lblA.TabIndex = 0;
+            lblA.Text = "Параметр a:";
+
+            txtA.Location = new Point(120, 17);
+            txtA.Name = "txtA";
+            txtA.Size = new Size(100, 23);
+            txtA.TabIndex = 1;
+
+            lblXn.AutoSize = true;
+            lblXn.Location = new Point(20, 55);
+            lblXn.Name = "lblXn";
+            lblXn.Size = new Size(95, 15);
+            lblXn.TabIndex = 2;
+            lblXn.Text = "Начало (xn):";
+
+            txtXn.Location = new Point(120, 52);
+            txtXn.Name = "txtXn";
+            txtXn.Size = new Size(100, 23);
+            txtXn.TabIndex = 3;
+            txtXn.Text = "3.35";
+
+            lblXk.AutoSize = true;
+            lblXk.Location = new Point(20, 90);
+            lblXk.Name = "lblXk";
+            lblXk.Size = new Size(85, 15);
+            lblXk.TabIndex = 4;
+            lblXk.Text = "Конец (xk):";
+
+            txtXk.Location = new Point(120, 87);
+            txtXk.Name = "txtXk";
+            txtXk.Size = new Size(100, 23);
+            txtXk.TabIndex = 5;
+            txtXk.Text = "36.26";
+
+            lblXh.AutoSize = true;
+            lblXh.Location = new Point(20, 125);
+            lblXh.Name = "lblXh";
+            lblXh.Size = new Size(70, 15);
+            lblXh.TabIndex = 6;
+            lblXh.Text = "Шаг (xh):";
+
+            txtXh.Location = new Point(120, 122);
+            txtXh.Name = "txtXh";
+            txtXh.Size = new Size(100, 23);
+            txtXh.TabIndex = 7;
+            txtXh.Text = "0.2";
+
+            btnCalculate.Location = new Point(20, 165);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(100, 30);
+            btnCalculate.TabIndex = 8;
+            btnCalculate.Text = "Вычислить";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+
+            btnClear.Location = new Point(130, 165);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(100, 30);
+            btnClear.TabIndex = 9;
+            btnClear.Text = "Очистить";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+
+            btnExit.Location = new Point(240, 165);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(100, 30);
+            btnExit.TabIndex = 10;
+            btnExit.Text = "Выход";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+
+            lblResults.AutoSize = true;
+            lblResults.Location = new Point(370, 20);
+            lblResults.Name = "lblResults";
+            lblResults.Size = new Size(120, 15);
+            lblResults.TabIndex = 11;
+            lblResults.Text = "Результаты:";
+
+            lstResults.FormattingEnabled = true;
+            lstResults.ItemHeight = 15;
+            lstResults.Location = new Point(370, 45);
+            lstResults.Name = "lstResults";
+            lstResults.Size = new Size(400, 289);
+            lstResults.TabIndex = 12;
+
+            lblMin.AutoSize = true;
+            lblMin.Location = new Point(370, 350);
+            lblMin.Name = "lblMin";
+            lblMin.Size = new Size(120, 15);
+            lblMin.TabIndex = 13;
+            lblMin.Text = "Минимум (f_min):";
+
+            txtMin.Location = new Point(500, 347);
+            txtMin.Name = "txtMin";
+            txtMin.ReadOnly = true;
+            txtMin.Size = new Size(270, 23);
+            txtMin.TabIndex = 14;
+
+            lblMax.AutoSize = true;
+            lblMax.Location = new Point(370, 385);
+            lblMax.Name = "lblMax";
+            lblMax.Size = new Size(125, 15);
+            lblMax.TabIndex = 15;
+            lblMax.Text = "Максимум (f_max):";
+
+            txtMax.Location = new Point(500, 382);
+            txtMax.Name = "txtMax";
+            txtMax.ReadOnly = true;
+            txtMax.Size = new Size(270, 23);
+            txtMax.TabIndex = 16;
+
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 430);
+            Controls.Add(txtMax);
+            Controls.Add(lblMax);
+            Controls.Add(txtMin);
+            Controls.Add(lblMin);
+            Controls.Add(lstResults);
+            Controls.Add(lblResults);
+            Controls.Add(btnExit);
+            Controls.Add(btnClear);
+            Controls.Add(btnCalculate);
+            Controls.Add(txtXh);
+            Controls.Add(lblXh);
+            Controls.Add(txtXk);
+            Controls.Add(lblXk);
+            Controls.Add(txtXn);
+            Controls.Add(lblXn);
+            Controls.Add(txtA);
+            Controls.Add(lblA);
+            Name = "Form1";
+            Text = "Вычисление функции с экстремумами";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblA;
+        private Label lblXn;
+        private Label lblXk;
+        private Label lblXh;
+        private TextBox txtA;
+        private TextBox txtXn;
+        private TextBox txtXk;
+        private TextBox txtXh;
+        private Button btnCalculate;
+        private Button btnClear;
+        private Button btnExit;
+        private ListBox lstResults;
+        private Label lblResults;
+        private Label lblMin;
+        private Label lblMax;
+        private TextBox txtMin;
+        private TextBox txtMax;
     }
 }
--- pm-labs/Form1.cs
+++ pm-labs/Form1.cs
@@ -6,5 +6,89 @@ namespace pm_labs
         {
             InitializeComponent();
         }
+
+        private double CalculateFunction(double x, double a)
+        {
+            double f;
+
+            if (x <= 0)
+            {
+                f = Math.Pow(Math.Abs(x), 5) * (1 / Math.Tan(x + 2));
+            }
+            else if (x > 0 && x <= a)
+            {
+                f = (5 * x + Math.Pow(x, 2)) / Math.Pow(Math.Pow(x, 2) + 3, 3);
+            }
+            else
+            {
+                f = Math.Pow(Math.Sin(x + 3), 2) / (Math.Pow(x, 5) - (1 / Math.Tan(Math.PI * Math.Pow(x, 3))));
+            }
+
+            return f;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(txtA.Text);
+                double xn = double.Parse(txtXn.Text);
+                double xk = double.Parse(txtXk.Text);
+                double xh = double.Parse(txtXh.Text);
+
+                lstResults.Items.Clear();
+                txtMin.Clear();
+                txtMax.Clear();
+
+                double x = xn;
+                double fMin = double.MaxValue;
+                double fMax = double.MinValue;
+                double xMin = xn;
+                double xMax = xn;
+
+                lstResults.Items.Add("     x          |        f(x)");
+                lstResults.Items.Add("----------------|-----------------");
+
+                while (x <= xk)
+                {
+                    double f = CalculateFunction(x, a);
+
+                    lstResults.Items.Add($"{x,15:F2} | {f,15:F6}");
+
+                    if (f < fMin)
+                    {
+                        fMin = f;
+                        xMin = x;
+                    }
+
+                    if (f > fMax)
+                    {
+                        fMax = f;
+                        xMax = x;
+                    }
+
+                    x += xh;
+                }
+
+                txtMin.Text = $"f({xMin:F2}) = {fMin:F6}";
+                txtMax.Text = $"f({xMax:F2}) = {fMax:F6}";
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка вычисления");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstResults.Items.Clear();
+            txtMin.Clear();
+            txtMax.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
