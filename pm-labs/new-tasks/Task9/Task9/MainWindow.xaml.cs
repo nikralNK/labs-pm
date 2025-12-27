bda6e04410954bec6a016992fee0ee9d818a2949
LotusBoard Admin <admin@lotusboard.local>
2025-12-27 07:26:15 +0330

    Add array shuffle functionality

--- pm-labs/new-tasks/Task9/Task9/MainWindow.xaml.cs
+++ pm-labs/new-tasks/Task9/Task9/MainWindow.xaml.cs
@@ -15,14 +15,62 @@ using System.Windows.Shapes;
 
 namespace Task9
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        int[] array;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+
+            string arrayText = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayText = arrayText + array[i];
+                if (i < array.Length - 1)
+                {
+                    arrayText = arrayText + ", ";
+                }
+            }
+
+            ArrayTextBox.Text = arrayText;
+            ResultTextBox.Text = "";
+        }
+
+        private void ShuffleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(0, i + 1);
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+
+            string resultText = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                resultText = resultText + array[i];
+                if (i < array.Length - 1)
+                {
+                    resultText = resultText + ", ";
+                }
+            }
+
+            ResultTextBox.Text = resultText;
         }
     }
 }
