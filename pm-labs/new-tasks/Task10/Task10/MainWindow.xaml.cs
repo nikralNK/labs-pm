32cfb1a7b0d55f6d53fed0936c58e0e49de2c644
LotusBoard Admin <admin@lotusboard.local>
2025-12-27 07:31:16 +0330

    Add neighbor average replacement

--- pm-labs/new-tasks/Task10/Task10/MainWindow.xaml.cs
+++ pm-labs/new-tasks/Task10/Task10/MainWindow.xaml.cs
@@ -15,14 +15,84 @@ using System.Windows.Shapes;
 
 namespace Task10
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        int[] array;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int size = int.Parse(SizeTextBox.Text);
+                Random random = new Random();
+                array = new int[size];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next(1, 100);
+                }
+
+                string arrayText = "";
+                for (int i = 0; i < array.Length; i++)
+                {
+                    arrayText = arrayText + array[i];
+                    if (i < array.Length - 1)
+                    {
+                        arrayText = arrayText + ", ";
+                    }
+                }
+
+                ArrayTextBox.Text = arrayText;
+                ResultTextBox.Text = "";
+            }
+            catch
+            {
+                ArrayTextBox.Text = "Ошибка ввода";
+            }
+        }
+
+        private void AverageButton_Click(object sender, RoutedEventArgs e)
+        {
+            int[] newArray = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int sum = array[i];
+                int count = 1;
+
+                if (i > 0)
+                {
+                    sum = sum + array[i - 1];
+                    count = count + 1;
+                }
+
+                if (i < array.Length - 1)
+                {
+                    sum = sum + array[i + 1];
+                    count = count + 1;
+                }
+
+                newArray[i] = sum / count;
+            }
+
+            array = newArray;
+
+            string resultText = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                resultText = resultText + array[i];
+                if (i < array.Length - 1)
+                {
+                    resultText = resultText + ", ";
+                }
+            }
+
+            ResultTextBox.Text = resultText;
         }
     }
 }
