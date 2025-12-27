8f89fa33285e52e7819219e42dbe31ffd2c269b2
LotusBoard Admin <admin@lotusboard.local>
2025-12-27 07:21:03 +0330

    Add array replace functionality

--- pm-labs/new-tasks/Task8/Task8/MainWindow.xaml.cs
+++ pm-labs/new-tasks/Task8/Task8/MainWindow.xaml.cs
@@ -15,14 +15,76 @@ using System.Windows.Shapes;
 
 namespace Task8
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        int[] array;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+
+            string arrayText = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayText = arrayText + array[i];
+                if (i < array.Length - 1)
+                {
+                    arrayText = arrayText + ", ";
+                }
+            }
+
+            ArrayTextBox.Text = arrayText;
+            ResultTextBox.Text = "";
+        }
+
+        private void ReplaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(NTextBox.Text);
+
+                int sum = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum = sum + array[i];
+                }
+                double average = (double)sum / array.Length;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] > n)
+                    {
+                        array[i] = (int)average;
+                    }
+                }
+
+                string resultText = "";
+                for (int i = 0; i < array.Length; i++)
+                {
+                    resultText = resultText + array[i];
+                    if (i < array.Length - 1)
+                    {
+                        resultText = resultText + ", ";
+                    }
+                }
+
+                ResultTextBox.Text = resultText;
+            }
+            catch
+            {
+                ResultTextBox.Text = "Ошибка ввода";
+            }
         }
     }
 }
