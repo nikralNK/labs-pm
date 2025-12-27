4092493a8a950f8953c62e319fe72033954f0f63
LotusBoard Admin <admin@lotusboard.local>
2025-12-27 06:21:25 +0330

    Add dynamic element creation

--- pm-labs/new-tasks/Task2/Task2/MainWindow.xaml.cs
+++ pm-labs/new-tasks/Task2/Task2/MainWindow.xaml.cs
@@ -15,14 +15,41 @@ using System.Windows.Shapes;
 
 namespace Task2
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CreateElement_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxRadio.IsChecked == true)
+            {
+                TextBox textBox = new TextBox();
+                textBox.Width = 100;
+                textBox.Height = 25;
+                textBox.Margin = new Thickness(5);
+                ElementsPanel.Children.Add(textBox);
+            }
+            else if (ButtonRadio.IsChecked == true)
+            {
+                Button button = new Button();
+                button.Content = "Кнопка";
+                button.Width = 100;
+                button.Height = 25;
+                button.Margin = new Thickness(5);
+                ElementsPanel.Children.Add(button);
+            }
+            else if (LabelRadio.IsChecked == true)
+            {
+                Label label = new Label();
+                label.Content = "Текст";
+                label.Width = 100;
+                label.Height = 25;
+                label.Margin = new Thickness(5);
+                ElementsPanel.Children.Add(label);
+            }
         }
     }
 }
